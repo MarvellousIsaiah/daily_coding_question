543bbbdecf01d96fbf1ff5b4198e413c4e5f9005
MarvellousIsaiah <isaiahugoeze@gmail.com>
2024-01-19 18:27:30 +0100

    added a new file

--- Name.py
+++ Name.py
@@ -1,4 +1,4 @@
 name = (input("what is your name: "))
-age = (input("what is your age: "))
+age = (input("what is your age:  "))
 print("hello " + name + ", welcome to my coding challenges world, your age is " + age)
 
